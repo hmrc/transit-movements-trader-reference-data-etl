d9560c0b195e82c6a0a51524b38cca3f8af730d8
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2021-07-09 13:58:47 +0100

    CTCTRADERS-2455 Add TODO to remove cross-cutting list logic

--- app/data/filter/FilterFlow.scala
+++ app/data/filter/FilterFlow.scala
@@ -34,6 +34,7 @@ case class FilterFlow(list: ReferenceDataList) {
 
   val logger: Logger = Logger(getClass.getSimpleName)
 
+  // TODO: Remove cross-cutting logic and filter using Transformation[ReferenceDataList]#filter
   private def isActiveRecord(state: String, activeFrom: Option[LocalDate]): Boolean =
     state == Common.valid && activeFrom
       .exists(_.isBefore(LocalDate.now().plusDays(1)))
