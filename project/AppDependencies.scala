6d439645394ee14c4a3d4acc16deaff87457491b
ericohwotu <23156971+ericohwotu@users.noreply.github.com>
2021-08-23 14:42:47 +0100

    [EO][CTCTRADERS-2605] update bootstrap for auth upgrade plus cleanup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,14 +8,8 @@ object AppDependencies {
   private val catsVersion = "2.1.1"
 
   val compile = Seq(
-    "uk.gov.hmrc"        %% "bootstrap-backend-play-27"          % "3.4.0",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-27"          % "5.12.0",
     "org.reactivemongo"  %% "play2-reactivemongo"                % "0.20.13-play27",
-    "uk.gov.hmrc"        %% "logback-json-logger"                % "5.1.0",
-    "uk.gov.hmrc"        %% "http-core"                          % "2.5.0",
-    "uk.gov.hmrc"        %% "http-verbs"                         % "12.3.0",
-    "uk.gov.hmrc"        %% "http-verbs-play-27"                 % "12.3.0",
-    "uk.gov.hmrc"        %% "crypto"                             % "6.0.0",
-    "uk.gov.hmrc"        %% "secure"                             % "8.0.0",
     "com.typesafe.play"  %% "play-iteratees"                     % "2.6.1",
     "com.typesafe.play"  %% "play-iteratees-reactive-streams"    % "2.6.1",
     "org.typelevel"      %% "cats-core"                          % catsVersion,
@@ -33,8 +27,7 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.typesafe.akka"      %% "akka-stream-testkit"      % "2.6.10",
     "com.typesafe.akka"      %% "akka-slf4j"               % "2.6.10",
-    "org.pegdown"            % "pegdown"                   % "1.6.0",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.35.10",
-    "com.github.tomakehurst" % "wiremock-standalone"       % "2.27.1"
+    "com.github.tomakehurst" % "wiremock-standalone"       % "2.27.2"
   ).map(_ % "test, it")
 }
