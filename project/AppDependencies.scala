4f64bf3ba672baaac4309b0bea1ed7babf51905e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-10-26 10:06:52 +0100

    CTCP-1525: Upgraded to Scala 2.13

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,15 @@
-import play.core.PlayVersion
-import play.core.PlayVersion.current
+import play.core.PlayVersion._
 import sbt._
 
 object AppDependencies {
 
   private val catsVersion = "2.8.0"
-  private val mongoVersion = "0.71.0"
+  private val mongoVersion = "0.73.0"
+  private val bootstrapVersion = "7.8.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"          % "7.3.0",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"          % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"                 % mongoVersion,
-    "com.typesafe.play"  %% "play-iteratees"                     % "2.6.1",
-    "com.typesafe.play"  %% "play-iteratees-reactive-streams"    % "2.6.1",
     "org.typelevel"      %% "cats-core"                          % catsVersion,
     "com.lightbend.akka" %% "akka-stream-alpakka-json-streaming" % "3.0.4",
     "com.enragedginger"  %% "akka-quartz-scheduler"              % "1.9.0-akka-2.6.x"
@@ -19,15 +17,15 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatest"          %% "scalatest"                % "3.2.12",
+    "org.scalatest"          %% "scalatest"                % "3.2.14",
     "com.typesafe.play"      %% "play-test"                % current,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.mockito"             % "mockito-core"             % "4.8.0",
-    "org.scalatestplus"      %% "mockito-4-5"              % "3.2.12.0",
-    "org.scalacheck"         %% "scalacheck"               % "1.16.0",
-    "org.scalatestplus"      %% "scalacheck-1-16"          % "3.2.12.0",
-    "com.typesafe.akka"      %% "akka-stream-testkit"      % PlayVersion.akkaVersion,
-    "com.typesafe.akka"      %% "akka-slf4j"               % PlayVersion.akkaVersion,
+    "org.scalatestplus"      %% "mockito-4-6"              % "3.2.14.0",
+    "org.scalacheck"         %% "scalacheck"               % "1.17.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"          % "3.2.14.0",
+    "com.typesafe.akka"      %% "akka-stream-testkit"      % akkaVersion,
+    "com.typesafe.akka"      %% "akka-slf4j"               % akkaVersion,
     "com.vladsch.flexmark"    % "flexmark-all"             % "0.62.2",
     "com.github.tomakehurst"  % "wiremock-standalone"      % "2.27.2"
   ).map(_ % "test, it")
